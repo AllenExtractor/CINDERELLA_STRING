4ee045737ae9f5487276686c8f54a3c459a63d83
AllenExtractor <dayarmustfa@gmail.com>
2026-05-19 18:06:16 +0530

    𝐓𝐞𝐚𝐦 𝐂𝐢𝐧𝐝𝐞𝐫𝐞𝐥𝐥𝐚

--- CINDERELLA/must_join.py
+++ CINDERELLA/must_join.py
@@ -24,7 +24,7 @@ async def must_join_channel(bot: Client, msg: Message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("🔶 ᴊᴏɪɴ ᴏғғɪᴄᴇ 🔶", url=link),
+                                InlineKeyboardButton("🔶ᴊᴏɪɴ ᴏғғɪᴄᴇ🔶", url=link),
                             ]
                         ]
                     )
