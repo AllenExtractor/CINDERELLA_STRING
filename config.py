b36d0aa9adef0010cdcb6d2d1c0755f1999b6553
AllenExtractor <dayarmustfa@gmail.com>
2026-06-25 18:46:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = int(getenv("API_ID", "33853339"))
 API_HASH = getenv("API_HASH", "d44e3a158d9da849df318173268f94c0")
 
 BOT_TOKEN = getenv("BOT_TOKEN", " ")
-OWNER_ID = int(getenv("OWNER_ID", "8446475678"))
+OWNER_ID = int(getenv("OWNER_ID", "8909902924"))
 
 MONGO_DB_URI = getenv("mongodb+srv://m49606145_db_user:Th15V5nu9utMejwO@cluster0.g11tftt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0.")
 MUST_JOIN = getenv("MUST_JOIN", "teamcinderella")
